73108a3993cada3b2085a51f5c740d8dd0af2fc4
informatica3vogue <informatica3vogue>
2017-11-28 11:23:25 -0600

    mantenimineto pajillas de toros

---
+++ reproduccion/Cpajillas.php
@@ -0,0 +1,53 @@
+<?php
+include '../plantilla.php';
+if($_POST){
+    $sql="insert into pajillas_toros values(default,'$_POST[toro]','$_POST[pajilla]','$_POST[tipo_semen]',true)";
+    $res=$conex->prepare($sql);
+  if($res->execute()){
+        $mensaje= '<div data-alert class="alert-box success round">
+ <h5 style="color:white">registro creado exitosamente</h5>
+  <a href="#" class="close">&times;</a>
+</div>';
+       }else{
+      $mensaje= '<div data-alert class="alert-box alert round">
+  <h5 style="color:white">Error al insertar el registro</h5>
+  <a href="#" class="close">&times;</a>
+</div>';
+        }
+}
+?>
+<div class="small-10 columns">
+        <?php echo $mensaje ?>
+        <h2>pajilla </h2>
+        <a href="pajillas.php" class="regresar">regresar</a>
+           <form action="" method="post" data-abide>
+    <div class="row">
+        <div class="small-4 columns">
+        <label>toro
+            <input type="text" name="toro" required="">               
+            <small class="error">requerido</small>
+        </label>
+            </div>
+            <div class="small-4 columns">
+        <label>codigo pajilla
+            <input type="text" name="pajilla" required="">
+            <small class="error">requerido</small>
+        </label>
+         </div>
+        <div class="small-4 columns">
+        <label>tipo semen
+            <select name="tipo_semen" required="">
+                <option value="">seleccione</option>
+                <option value="sexado">sexado</option>
+                <option value="convencional">convencional</option>
+            </select>
+            <small class="error">requerido</small>
+        </label>
+         </div>
+        
+
+</div>
+        <input type="submit" value="crear registro" class="button primary">
+    </form>
+</div>
+</div>
--- reproduccion/pajillas.php
+++ reproduccion/pajillas.php
@@ -1,18 +1,86 @@
+<?php    include '../plantilla.php'; ?>
+
+
+ <div class="small-10 columns">
+<h2>admon pajillas</h2>
+
+<a href="Cpajillas.php" class="button primary">ingresar pajilla</a>
+
+
 <?php
-include '../plantilla.php';
-
+$res=$conex->query("select * from pajillas_toros");
 ?>
-<div class="small-10 columns">
-    <h2>pajilla </h2>
-    <form>
-        <label>toro
-        <input type="text" name="toro">
-        </label>
-        <label>codigo pajilla
-        <input type="text" name="pajilla">
-        </label>
-        <input type="submit" value="crear registro" class="button primary">
-    </form>
-</div>
 
+<table class="table" data-filtering='true' data-paging="true">
+	<thead>
+		<tr>
+			<th>toro</th>
+			<th>codigo pajilla</th>
+			<th>tipo semen</th>
+			<th>disponible</th>			
+                        <th data-filterable="false">acciones</th>
+		</tr>
+	</thead>
+	<tbody>
+<?php
+while($fila=$res->fetch()){
+    
+    
+    ?>
+            <tr>
+                <td><?php  echo $fila[codigo_toro]?></td>
+                <td><?php  echo $fila[codigo_pajilla]?></td>
+                <td><?php  echo $fila[tipo_semen]?></td>
+                <td><?php  echo $fila[disponible]?'si':'no' ?></td>
+   
+                <td>
+                      <a href="#" class="ver" data-id="<?php  echo base64_encode( $fila[id]) ?>"><i class="fa fa-eye" aria-hidden="true"></i></a>
+                    <!--<a href="Upalpaciones.php?<?php  echo  base64_encode($fila[id])?>"><i class="fa fa-pencil" aria-hidden="true"></i></a>-->                    
+                    <a href="Dpalpaciones.php?<?php  echo  base64_encode('palpacion='. $fila[id])?>" id="eliminar"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                </td>
+            </tr>
+            
+    <?php
+}
+?>	
+
+	</tbody>
+</table>
+
+<div id="mimodal" class="reveal-modal"  data-reveal >
+    <span></span>
+    <a class="close-reveal-modal" aria-label="Close">&#215;</a>
 </div>
+</div>
+</div>
+<script>
+
+
+    
+                        $(".table").on('click','a.ver',function(e){
+        e.preventDefault();
+        
+        $.ajax({
+            url:"Rpalpaciones.php?"+$(this).data('id'),
+            success:function (datos){
+                console.log(datos);
+                   $('#mimodal span').html(datos);
+            }
+        });
+
+     
+        $('#mimodal').foundation('reveal', 'open');
+
+    });
+    
+    //////////////////eliminar
+        $("a#eliminar").on('click',function(e){
+        
+        e.preventDefault();
+       if( confirm("desea eliminar?")){
+           window.location=$(this).attr('href');
+       }
+    });
+
+    $(".table").footable();
+</script>
