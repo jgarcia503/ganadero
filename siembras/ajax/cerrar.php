b49d06815236102ea8e540650879cdff63cbf463
informatica3vogue <informatica3vogue>
2017-12-08 17:26:20 -0600

    modificar cerra proyecto para que no de error cuando haya multiples tablones

--- siembras/ajax/cerrar.php
+++ siembras/ajax/cerrar.php
@@ -6,9 +6,9 @@ $tmp=  explode('&', $_SERVER[QUERY_STRING]);
 $consultas[]="update proyectos_enc set cerrado='true',fecha_fin=current_date where id_proyecto=$tmp[0]";
 
 if($tmp[1]==='true'){
-$consultas[]="update tablones set estatus='libre' where id in (select id_tablones::integer from proyecto_tablones where id_proyecto='$tmp[0]')";
+$consultas[]="update tablones set estatus='libre' where id::text in (select regexp_split_to_table(id_tablones,',') from proyecto_tablones where id_proyecto='$tmp[0]')";
 }else{
-    $consultas[]="update tablones set estatus='descansando' where id in (select id_tablones::integer from proyecto_tablones where id_proyecto='$tmp[0]')";
+    $consultas[]="update tablones set estatus='descansando' where id::text in (select regexp_split_to_table(id_tablones,',') from proyecto_tablones where id_proyecto='$tmp[0]')";
 }
 
 try{
