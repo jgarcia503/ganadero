9cad6ca92b85862c2b5273d9947eac0d9a34ec5e
informatica3vogue <informatica3vogue>
2018-01-30 15:34:55 -0600

    descargar de inventarios los materiales usados en un servicio de vaca segun la plantilla de requisiciones

--- reproduccion/Cservicios.php
+++ reproduccion/Cservicios.php
@@ -3,27 +3,61 @@ $hembras=$conex->query("select * from animales where sexo='Hembra' and estado no
 $machos=$conex->query("select * from animales where sexo='Macho'");
 $pajillas=$conex->query("select * from pajillas_toros where disponible =true");
 $cat_servicio=$conex->query("select * from cat_tipos_servicios");
+$bodegas=$conex->query("select * from bodega");
+
 if($_POST){
-$reload='window.location.reload';
- $insert =$conex->prepare("insert into servicios"
-         . " values(default,'$_POST[fecha]','$_POST[hora]','$_POST[animal]','$_POST[tipo]','$_POST[inseminador]'"
-         . ",'$_POST[padre]','$_POST[donadora]',trim('$_POST[notas]'),'$_POST[cod_pajilla]','$_POST[hora_visualizacion_celo]')");
-     if($insert->execute()){
-         $sql_update="update pajillas_toros set disponible=false where codigo_pajilla='$_POST[cod_pajilla]'";
-         $res=$conex->prepare($sql_update);
-         if($res->execute()){
-             $mensaje= '<div data-alert class="alert-box success round">
-                                <h5 style="color:white">registro creado exitosamente</h5>
-                                <a href="#" class="close">&times;</a>
-                                </div>';
-         }else{
-      $mensaje= '<div data-alert class="alert-box alert round">
-                            <h5 style="color:white">Error al insertar el registro</h5>
-                             <a href="#" class="close">&times;</a>
-                               </div>';
+    extract($_POST);
+    include '../php funciones/funciones.php';
+    $lineas_prods=$conex->query("select * from plantilla_servicios_requisicion_lns a join productos b on b.referencia=a.producto_id where a.enc_id=$tipo");
+    $copia_vals='';
+    $total=0;
+$subtotal=0;
+$consultas=[];
+try{
+    $conex->beginTransaction();
+    $insert1 = $conex->prepare("insert into servicios  values(default,'$fecha','$hora','$animal','$tipo','$inseminador','$padre','$donadora',trim('$notas'),'$cod_pajilla','$hora_visualizacion_celo') returning id");
+        if ($insert1->execute()) {
+            $ultimo_id = $insert1->fetchColumn();
+            $insert2 = $conex->prepare("insert into servicios_requisicion_enc  values(default,'$tipo','$_SESSION[usuario]',now()::text,'$ultimo_id') returning id");
+            if ($insert2->execute()) {
+                $ultimo_id = $insert2->fetchColumn();
+                $sql3 = "insert into servicios_requisicion_lns  values ";
+                while ($fila = $lineas_prods->fetch()) {
+                    $cant_conv = convertir($fila[unidad], $fila[cantidad]);
+                    $total+=($cant_conv * $fila[precio_promedio]);
+                    $subtotal = ($cant_conv * $fila[precio_promedio]);
+                    $copia_vals.="(default,'" . $fila[producto_id] . "','" . $fila[cantidad] . "','" . $fila[unidad] . "','$ultimo_id','$fila[precio_promedio]','$subtotal'),";
+
+                    #########################################################
+                    $consultas[] = "update existencias set existencia=existencia::numeric(1000,2)-$cant_conv "
+                            . "where codigo_bodega='$bodega' "
+                            . "and codigo_producto='$fila[producto_id]'";
+
+                    $consultas[] = "update productos set cantidad_total=(cantidad_total::numeric(1000,2)-$cant_conv) where referencia='$fila[producto_id]'";
+
+                    $consultas[] = "insert into kardex values (default,'$bodega','$fila[producto_id]',now(),'requisicion-servicio','$ultimo_id','$fila[precio_promedio]','','$cant_conv')";
+                }
+                $consultas[] = trim($sql3 . $copia_vals, ',');
             }
-        
-    } 
+        }
+        foreach ($consultas as $value){
+                if(!$conex->prepare($value)->execute()){
+                                    throw new PDOException();                                           
+                  }
+        }
+            $conex->commit();
+            $mensaje= '<div data-alert class="alert-box success round">
+ <h5 style="color:white">registro creado exitosamente</h5>
+  <a href="#" class="close">&times;</a>
+</div>';
+    }  
+catch (PDOException $pe){
+    $conex->rollBack();
+              $mensaje= '<div data-alert class="alert-box alert round">
+  <h5 style="color:white">Error al insertar el registro</h5>
+  <a href="#" class="close">&times;</a>
+</div>';
+    }
 }
 ?>
 
@@ -82,7 +116,19 @@ $reload='window.location.reload';
                 ?>
             </select>
         </div>
-
+        <div class="small-2 columns">
+            <label>
+                bodega
+                            <select name="bodega">
+                <option value="">seleccione</option>
+                 <?php
+                        while($fila=$bodegas->fetch()){
+                            echo "<option value='$fila[codigo]'>$fila[nombre]</option>";
+                        }
+                        ?>
+            </select>
+            </label>
+        </div>
     </div>
     <div class="row">
         
@@ -139,7 +185,7 @@ $reload='window.location.reload';
 </div>
 </div>
 <script>
-        $("select").on('change',function(){
+        $("select[name=tipo]").on('change',function(){
             
             switch($(this).val()){
                 case '1':
